7db82471b45f4965a50bcbf05e85e6e6efe7e082
jj <nn>
2023-09-25 17:43:02 +1300

    First commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#041f27f5]'> 
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-white-600 text-[#8892b0]'>Hi, I'm</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Graylin Chawanji
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          Full-stack Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+         with a passion for crafting visually appealing and highly functional web applications. 
+        </p>
+        <div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+import WorkIm from '../assets/projects/workingImg.png'
+import RealEst from '../assets/projects/realestate.jpg'
+import Restaurant from '../assets/projects/Restaurant.png'
+
+export const data=[
+    {
+        id:1,
+        name:"WeatherMe",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"PropertyMe",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"KG Indian",
+        image:Restaurant,
+        github:"",
+        live:"",
+    }
+   
+   
+
+
+]
